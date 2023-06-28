228049914ca9cf8b46806858198e3d5cf90adfa7
Fredrik Orderud <fredrik.orderud@ge.com>
2023-06-28 17:15:09 +0200

    Update comment.

--- IntelliMouse.ps1
+++ IntelliMouse.ps1
@@ -6,7 +6,7 @@ $mouse = Get-CimInstance -Namespace root\WMI -Class FireflyDeviceInformation
 # check if mouse is active
 $mouse.Active
 
-# get tail-light status
+# get current tail-light color
 $mouse.TailLight
 
 # update tail-light (doesn't work)
