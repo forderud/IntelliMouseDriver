faedd0916a399179b7984cc7ffdadef128d6f33f
Fredrik Orderud <fredrik.orderud@ge.com>
2023-06-28 17:26:16 +0200

    Working WMI interface baaseline.

--- IntelliMouse.ps1
+++ IntelliMouse.ps1
@@ -1,6 +1,6 @@
-# Must be run with admin privileges!
+# Must be run with admin privileges
 
-# query IntelliMouse WMI class
+# get IntelliMouse WMI object
 $mouse = Get-CimInstance -Namespace root\WMI -Class FireflyDeviceInformation
 
 # check if mouse is active
@@ -9,5 +9,6 @@ $mouse.Active
 # get current tail-light color
 $mouse.TailLight
 
-# update tail-light (doesn't work)
-$mouse.TailLight = 0
+# set tail-light to blue
+$mouse.TailLight = 0xFF0000 # 0xBBGGRR format
+Set-CimInstance -CimInstance $mouse -PassThru
