8631b044279a6fd090148d54972981cbd170dd1e
Fredrik Orderud <fredrik.orderud@ge.com>
2023-07-12 00:54:38 +0200

    Remove unreferenced FireflyDeviceInformation WmiInstance member from DEVICE_CONTEXT

--- firefly/device.h
+++ firefly/device.h
@@ -2,11 +2,7 @@
 // a WDM device extension in the driver framework
 typedef struct _DEVICE_CONTEXT
 {
-    // Our WMI data generated from firefly.mof
-    FireflyDeviceInformation WmiInstance;
-
     UNICODE_STRING PdoName;
-
 } DEVICE_CONTEXT, *PDEVICE_CONTEXT;
 
 WDF_DECLARE_CONTEXT_TYPE(DEVICE_CONTEXT)
