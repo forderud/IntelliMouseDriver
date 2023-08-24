e2ed300b669d8998a1577c882ad16c01502d86cf
Fredrik Orderud <fredrik.orderud@ge.com>
2023-08-24 18:25:11 +0200

    Complete devgen replacement command.
    Still doesn't work though..

--- TailLight/INSTALL.bat
+++ TailLight/INSTALL.bat
@@ -6,7 +6,7 @@ cd /d "%~dp0"
 devcon /r install TailLight.inf "HID\VID_045E&PID_082A&MI_01&Col05"
 
 :: Use PnpUtil for installation (succeeds but driver isn't loaded)
-::devgen /add /bus ROOT
+::devgen /add /bus ROOT /hardwareid "HID\VID_045E&PID_082A&MI_01&Col05"
 ::PNPUTIL /add-driver TailLight.inf /install /reboot
 
 pause
