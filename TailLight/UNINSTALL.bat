df803075a7d977b2f9466c224ca60391e1757a3d
Fredrik Orderud <fredrik.orderud@ge.com>
2023-08-27 19:33:47 +0200

    Add DevCon step to also delete the devnode.

--- TailLight/UNINSTALL.bat
+++ TailLight/UNINSTALL.bat
@@ -2,6 +2,13 @@
 :: Goto current directory
 cd /d "%~dp0"
 
+:: Use DevCon to delete the devnode
+devcon /r remove "HID\VID_045E&PID_082A&MI_01&Col05"
+:: Use DevCon for uninstallation (fails)
+::devcon -f dp_delete TailLight.inf
+
+:: Use PnpUtil for uninstallation (succeeds but devnode leaks)
 PNPUTIL /delete-driver TailLight.inf /uninstall /force /reboot
+::PNPUTIL /remove-device "HID\VID_045E&PID_082A&MI_01&Col05"
 
 pause
