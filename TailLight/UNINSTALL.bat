5b073312835e7f3474d82c5bc8eb140479f93c68
Fredrik Orderud <fredrik.orderud@ge.com>
2023-09-13 22:53:28 +0200

    Add TODO for switching to PNPUTIL in Win11 21H2.

--- TailLight/UNINSTALL.bat
+++ TailLight/UNINSTALL.bat
@@ -2,14 +2,13 @@
 :: Goto current directory
 cd /d "%~dp0"
 
-:: Use DevCon to delete the devnode
+:: delete the devnode
 devcon /r remove "HID\VID_045E&PID_082A&MI_01&Col05"
-:: Use DevCon for uninstallation (fails)
-::devcon -f dp_delete TailLight.inf
+:: TODO: Switch to PNPUTIL in Win11 21H2
+::PNPUTIL /remove-device /deviceid "HID\VID_045E&PID_082A&MI_01&Col05"
 
-:: Use PnpUtil for uninstallation (succeeds but devnode leaks)
+:: uninstall driver
 PNPUTIL /delete-driver TailLight.inf /uninstall /force /reboot
-::PNPUTIL /remove-device "HID\VID_045E&PID_082A&MI_01&Col05"
 
 :: Delete TailLightDeviceInformation WMI class security descriptor
 reg delete "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Security" /v "07982702-2086-4B83-9444-34989BB10554" /f
