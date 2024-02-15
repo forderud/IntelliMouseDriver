bd1e47b6d717eff80eb97030be17acde35ea1f84
Fredrik Orderud <fredrik.orderud@ge.com>
2024-02-15 09:51:47 +0100

    Update USB identifiers to the actual IntelliMouse "mouse" device
    Microsoft IntelliMouse is a composite device that exposes multiple interfaces, including the mouse and tail-light. This changes the driver to attach towards the mouse instead of the tail-light.

--- MouseMirror/INSTALL_MouseMirror.bat
+++ MouseMirror/INSTALL_MouseMirror.bat
@@ -3,10 +3,10 @@
 cd /d "%~dp0"
 
 :: Use DevCon for installation, since it allows providing HWID
-devcon /r install MouseMirror.inf "HID\VID_045E&PID_082A&MI_01&Col05"
+devcon /r install MouseMirror.inf "HID\VID_045E&PID_082A&MI_00&Col01"
 
 :: Use PnpUtil for installation (succeeds but driver isn't loaded)
-::devgen /add /bus ROOT /hardwareid "HID\VID_045E&PID_082A&MI_01&Col05"
+::devgen /add /bus ROOT /hardwareid "HID\VID_045E&PID_082A&MI_00&Col01"
 ::PNPUTIL /add-driver MouseMirror.inf /install /reboot
 
 pause
