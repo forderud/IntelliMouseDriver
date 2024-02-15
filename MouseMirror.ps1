46254f7c34ae8f2714d251acf3a679560644e25b
Fredrik Orderud <fredrik.orderud@ge.com>
2024-02-15 11:06:27 +0100

    Reduce whitespace.

--- MouseMirror.ps1
+++ MouseMirror.ps1
@@ -3,10 +3,7 @@
 $mouse = Get-CimInstance -Namespace root\WMI -Class MouseMirrorDeviceInformation
 
 Write-Host("IntelliMouse device:")
-
 Write-Host("  InstanceName: {0}" -f $mouse.InstanceName)
-
-# check if mouse is active
 Write-Host("  Active: {0}" -f $mouse.Active)
 
 Write-Host("  Flipping: LeftRight={0}, UpDown={1}" -f $mouse.FlipLeftRight, $mouse.FlipUpDown)
