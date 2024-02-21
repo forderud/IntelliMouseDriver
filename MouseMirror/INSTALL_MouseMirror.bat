31deac7412c32b246ac5edac7b941a8943d9aeae
Fredrik Orderud <fredrik.orderud@ge.com>
2024-02-21 21:24:23 +0100

    Auto-install driver certificate.

--- MouseMirror/INSTALL_MouseMirror.bat
+++ MouseMirror/INSTALL_MouseMirror.bat
@@ -3,8 +3,8 @@
 cd /d "%~dp0"
 
 :: Install certificate for silent driver installation and loading (run from administrative command prompt)
-:: certmgr.exe /add MouseMirror.cer /s /r localMachine root
-:: certmgr.exe /add MouseMirror.cer /s /r localMachine trustedpublisher
+certmgr.exe /add MouseMirror.cer /s /r localMachine root
+certmgr.exe /add MouseMirror.cer /s /r localMachine trustedpublisher
 
 :: Use PnpUtil for installation
 PNPUTIL /add-driver MouseMirror.inf /install /reboot
