39656a0c074366bd409b2d65c2d2a7f83473a288
Fredrik Orderud <fredrik.orderud@ge.com>
2024-03-05 12:48:02 +0100

    Convert DEVICE_CONTEXT structs into a C++ struct.

--- MouseMirror/device.h
+++ MouseMirror/device.h
@@ -2,12 +2,11 @@
 #include <kbdmou.h>
 
 /** Driver-specific struct for storing instance-specific data. */
-typedef struct _DEVICE_CONTEXT {
+struct DEVICE_CONTEXT {
     UNICODE_STRING PdoName;
     WDFWMIINSTANCE WmiInstance;
     CONNECT_DATA   UpperConnectData; // callback to intercept mouse packets
-} DEVICE_CONTEXT;
-
+};
 WDF_DECLARE_CONTEXT_TYPE(DEVICE_CONTEXT)
 
 WDF_DECLARE_CONTEXT_TYPE(MouseMirrorDeviceInformation)
--- TailLight/device.h
+++ TailLight/device.h
@@ -1,11 +1,10 @@
 #pragma once
 
 /** Driver-specific struct for storing instance-specific data. */
-typedef struct _DEVICE_CONTEXT {
+struct DEVICE_CONTEXT {
     UNICODE_STRING PdoName;
     WDFWMIINSTANCE WmiInstance;
-} DEVICE_CONTEXT;
-
+};
 WDF_DECLARE_CONTEXT_TYPE(DEVICE_CONTEXT)
 
 WDF_DECLARE_CONTEXT_TYPE(TailLightDeviceInformation)
