5a479d7ca3d1f29dfd24e7d4f0827792ec2cd4ec
ManOnTheMountainTech <mindinsky@gmail.com>
2024-03-27 12:50:12 -0700

    Merged files with main

--- TailLight/driver.cpp
+++ TailLight/driver.cpp
@@ -5,20 +5,21 @@
 NTSTATUS DriverEntry(
     _In_ PDRIVER_OBJECT  DriverObject,
     _In_ PUNICODE_STRING RegistryPath
-)
+    )
 {
     KdPrint(("TailLight: DriverEntry - WDF version built on %s %s\n", __DATE__, __TIME__));
 
     WDF_DRIVER_CONFIG params = {};
     WDF_DRIVER_CONFIG_INIT(/*out*/&params, EvtDriverDeviceAdd);
+    params.DriverPoolTag = POOL_TAG;
     params.EvtDriverUnload = EvtDriverUnload;
 
     // Create the framework WDFDRIVER object, with the handle to it returned in Driver.
-    NTSTATUS status = WdfDriverCreate(DriverObject,
-        RegistryPath,
-        WDF_NO_OBJECT_ATTRIBUTES,
-        &params,
-        WDF_NO_HANDLE); // [out]
+    NTSTATUS status = WdfDriverCreate(DriverObject, 
+                             RegistryPath, 
+                             WDF_NO_OBJECT_ATTRIBUTES, 
+                             &params, 
+                             WDF_NO_HANDLE); // [out]
     if (!NT_SUCCESS(status)) {
         // Framework will automatically cleanup on error Status return
         KdPrint(("TailLight: Error Creating WDFDRIVER 0x%x\n", status));
@@ -32,7 +33,7 @@ NTSTATUS DriverEntry(
     Used to perform operations that must take place before the driver is unloaded.  */
 VOID EvtDriverUnload(
     _In_ WDFDRIVER Driver
-)
+    )
 {
     UNREFERENCED_PARAMETER(Driver);
     KdPrint(("TailLight: DriverUnload.\n"));
