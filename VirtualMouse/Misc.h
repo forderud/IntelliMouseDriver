e2adf638daacbb87ae132e88193574b1563d59ea
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-05-06 22:21:59 +0200

    Delete dead RW queue code.

--- VirtualMouse/Misc.h
+++ VirtualMouse/Misc.h
@@ -5,19 +5,6 @@
 #include "trace.h"
 
 
-struct BUFFER_CONTENT {
-    LIST_ENTRY  BufferLink;
-    SIZE_T      BufferLength;
-    UCHAR       BufferStart; // variable-size structure, first byte of last field
-};
-
-
-struct WRITE_BUFFER_TO_READ_REQUEST_QUEUE {
-    LIST_ENTRY WriteBufferQueue; // write data comes in, keep it here to complete a read request
-    WDFQUEUE   ReadBufferQueue; // read request comes in, stays here til a matching write buffer arrives
-    WDFSPINLOCK qsync;
-};
-
 /** C++ spin lock RAII wrapper. */
 class SpinLock {
 public:
@@ -31,15 +18,3 @@ public:
 private:
     WDFSPINLOCK m_lock;
 };
-
-NTSTATUS
-WRQueueInit(
-    _In_    WDFDEVICE parent,
-    _Inout_ WRITE_BUFFER_TO_READ_REQUEST_QUEUE* pQ,
-    _In_    BOOLEAN bUSBReqQueue
-);
-
-VOID
-WRQueueDestroy(
-    _Inout_ WRITE_BUFFER_TO_READ_REQUEST_QUEUE* pQ
-);
