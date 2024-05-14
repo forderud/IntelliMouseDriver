forderud/IntelliMouseDriver

commit b615de3405de4ee20ee1f27082afb7d06657e7f7

Fix WdfTimerStart return-type mismatch.

TailLight/device.cpp
@@ -36,10 +36,10 @@ NTSTATUS EvtSelfManagedIoInit(WDFDEVICE device) {
         return status;
     }
 
-    status = WdfTimerStart(timer, 0); // no wait
-    if (!NT_SUCCESS(status)) {
+    BOOLEAN ok = WdfTimerStart(timer, 0); // no wait
+    if (!ok) {
         KdPrint(("WdfTimerStart failed 0x%x\n", status));
-        return status;
+        return STATUS_UNSUCCESSFUL;
     }
 
     return status;

commit a2a7abf21d48bbf43b147f01ec391f1e3fd177f1

Fix incorrect timer return-value check
Fixes regression introduced in b615de3405de4ee20ee1f27082afb7d06657e7f7.

TailLight/device.cpp
@@ -36,11 +36,8 @@ NTSTATUS EvtSelfManagedIoInit(WDFDEVICE device) {
         return status;
     }
 
-    BOOLEAN ok = WdfTimerStart(timer, 0); // no wait
-    if (!ok) {
-        KdPrint(("WdfTimerStart failed 0x%x\n", status));
-        return STATUS_UNSUCCESSFUL;
-    }
+    BOOLEAN inQueue = WdfTimerStart(timer, 0); // no wait
+    NT_ASSERTMSG("TailLight: timer already in queue", !inQueue);
 
     return status;
 }