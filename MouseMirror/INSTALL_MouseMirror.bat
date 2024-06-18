18fd54a1c9a225db1d05d8f4d377651b0c6b715a
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-06-18 16:47:40 +0200

    Fix cert install failure on clean VM without any existing "Trusted Publisher" certs
    Port of https://github.com/forderud/BatterySimulator/pull/22 to this repo.

--- MouseMirror/INSTALL_MouseMirror.bat
+++ MouseMirror/INSTALL_MouseMirror.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 
 :: Trust driver certificate
 certutil.exe -addstore root MouseMirror.cer
-certutil.exe -addstore trustedpublisher MouseMirror.cer
+certutil.exe -f -addstore trustedpublisher MouseMirror.cer
 
 :: Use PnpUtil for installation
 PNPUTIL /add-driver MouseMirror.inf /install
--- TailLight/INSTALL_TailLight.bat
+++ TailLight/INSTALL_TailLight.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 
 :: Trust driver certificate
 certutil.exe -addstore root TailLight.cer
-certutil.exe -addstore trustedpublisher TailLight.cer
+certutil.exe -f -addstore trustedpublisher TailLight.cer
 
 :: Use PnpUtil for installation
 PNPUTIL /add-driver TailLight.inf /install
