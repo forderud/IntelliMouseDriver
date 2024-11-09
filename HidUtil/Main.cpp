8391e06ea56af027cbc2ac3a88d62f6f15a39d21
Fredrik Orderud <forderud@gmail.com>
2024-11-09 16:36:10 +0100

    Change HID query to detect UPS Power Devices

--- HidUtil/Main.cpp
+++ HidUtil/Main.cpp
@@ -10,6 +10,32 @@ void PrintReport(const std::vector<BYTE>& report) {
     wprintf(L"}\n");
 }
 
+struct StatusReport {
+    BYTE reportId = 0x07; // reportId prefix
+
+    uint8_t Charging : 1;                   // bit 0x00
+    uint8_t Discharging : 1;                // bit 0x01
+    uint8_t ACPresent : 1;                  // bit 0x02
+
+    BYTE padding[2];
+};
+
+struct RemainingCapReport {
+    BYTE reportId = 0x0C; // reportId prefix
+    BYTE data[3] = {}; // 24 bit integer
+
+    uint32_t Value() const {
+        return (data[2] << 16) | (data[1] << 8) | data[0];
+    }
+};
+
+struct ChemistryReport {
+    BYTE reportId = 0x1F; // reportId prefix
+    BYTE idx = 0; // string index
+    BYTE padding[2] = {};
+};
+
+
 int main(int argc, char* argv[]) {
     if (argc < 4) {
         wprintf(L"IntelliMouse tail-light shifter.\n");
@@ -22,11 +48,8 @@ int main(int argc, char* argv[]) {
     BYTE blue = (BYTE)atoi(argv[3]);
 
     hid::Scan::Criterion crit;
-    //crit.VendorID = 0x045E;  // Microsoft
-    //crit.ProductID = 0x082A; // Pro IntelliMouse
-    //crit.ProductID = 0x0047; // IntelliMouse Explorer 3.0 (access denied)
-    crit.Usage = 0x0212;     //
-    crit.UsagePage = 0xFF07; // Vendor-defined range (FF00-FFFF)
+    crit.Usage = 0x0004;     // UPS
+    crit.UsagePage = 0x0084; // Power Device
 
     wprintf(L"Searching for matching HID devices...\n");
     std::vector<hid::Device> matches = hid::Scan::FindDevices(crit, false);
@@ -63,6 +86,16 @@ int main(int argc, char* argv[]) {
         if ((dev.caps.Usage == 0x0212) && (dev.caps.UsagePage == 0xFF07)) {
             // Pro IntelliMouse detected
             SetTailLightRGB(dev, RGB(red, green, blue));
+        } else {
+            auto status = dev.GetReport<StatusReport>(HidP_Feature);
+            wprintf(L"Charging: %s\n", status.Charging ? L"yes" : L"no");
+
+            auto fullCap = dev.GetReport<RemainingCapReport>(HidP_Feature);
+            wprintf(L"Remaining capacity: %u AmpSec\n", fullCap.Value());
+
+            auto chemIdx = dev.GetReport< ChemistryReport>(HidP_Feature);
+            std::wstring chemStr = dev.GetString(chemIdx.idx);
+            wprintf(L"Chemistry: %s\n", chemStr.c_str());
         }
     }
 
