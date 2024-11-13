4b330ce9aa3acf2e31f8e9e3cecd67b5358037df
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-11-13 20:18:20 +0100

    Reorder projects alphabetically.

--- BuildRelease.ps1
+++ BuildRelease.ps1
@@ -17,25 +17,25 @@ if ($LastExitCode -ne 0) {
 ### Package drivers, test-utilities and scripts
 New-Item -Name dist -ItemType "directory"
 
-# Compress TailLight driver
-Compress-Archive -Path x64\Release\TailLight   -DestinationPath dist\TailLight_x64.zip
-Compress-Archive -Path ARM64\Release\TailLight -DestinationPath dist\TailLight_arm64.zip
 # Compress MouseMirror driver
 Compress-Archive -Path x64\Release\MouseMirror   -DestinationPath dist\MouseMirror_x64.zip
 Compress-Archive -Path ARM64\Release\MouseMirror -DestinationPath dist\MouseMirror_arm64.zip
+# Compress TailLight driver
+Compress-Archive -Path x64\Release\TailLight   -DestinationPath dist\TailLight_x64.zip
+Compress-Archive -Path ARM64\Release\TailLight -DestinationPath dist\TailLight_arm64.zip
 # Compress VirtualMouse driver
 Compress-Archive -Path x64\Release\VirtualMouse   -DestinationPath dist\VirtualMouse_x64.zip
 Compress-Archive -Path ARM64\Release\VirtualMouse -DestinationPath dist\VirtualMouse_arm64.zip
 
-# Copy MouseMove utility
-Copy-Item -Path x64\Release\MouseMove.exe   -Destination dist\MouseMove_x64.zip
-Copy-Item -Path arm64\Release\MouseMove.exe -Destination dist\MouseMove_arm64.zip
-# Copy HidUtil utility
-Copy-Item -Path x64\Release\HidUtil.exe   -Destination dist\HidUtil_x64.zip
-Copy-Item -Path arm64\Release\HidUtil.exe -Destination dist\HidUtil_arm64.zip
 # Copy flicker utility
 Copy-Item -Path x64\Release\flicker.exe   -Destination dist\flicker_x64.zip
 Copy-Item -Path arm64\Release\flicker.exe -Destination dist\flicker_arm64.zip
+# Copy HidUtil utility
+Copy-Item -Path x64\Release\HidUtil.exe   -Destination dist\HidUtil_x64.zip
+Copy-Item -Path arm64\Release\HidUtil.exe -Destination dist\HidUtil_arm64.zip
+# Copy MouseMove utility
+Copy-Item -Path x64\Release\MouseMove.exe   -Destination dist\MouseMove_x64.zip
+Copy-Item -Path arm64\Release\MouseMove.exe -Destination dist\MouseMove_arm64.zip
 
 # Copy test scripts
 Copy-Item -Path x64\Release\*.ps1 -Destination dist
