b4e561f4567066d9dd6157a63e3560a229434f06
Fredrik Orderud <forderud@gmail.com>
2025-01-08 17:03:19 +0100

    Sync over update from https://github.com/forderud/BatterySimulator repo

--- HidUtil/HID.hpp
+++ HidUtil/HID.hpp
@@ -6,6 +6,7 @@
 #include <comdef.h>
 #include <wrl/wrappers/corewrappers.h> // for FileHandle RAII wrapper
 
+#include <algorithm> // for std::sort
 #include <cassert>
 #include <string>
 #include <vector>
@@ -207,7 +208,7 @@ public:
         if (valueCapsLen == 0)
             return {};
 
-        std::vector<HIDP_VALUE_CAPS> valueCaps(valueCapsLen, HIDP_VALUE_CAPS{});
+        std::vector<HIDP_VALUE_CAPS> valueCaps(valueCapsLen, {});
         NTSTATUS status = HidP_GetValueCaps(type, valueCaps.data(), &valueCapsLen, preparsed);
         if (status == HIDP_STATUS_INVALID_PREPARSED_DATA) {
             wprintf(L"WARNING: Invalid preparsed data.\n");
@@ -216,6 +217,42 @@ public:
 
         assert(status == HIDP_STATUS_SUCCESS); status;
         return valueCaps;
+    }
+
+    std::vector<HIDP_BUTTON_CAPS> GetButtonCaps(HIDP_REPORT_TYPE type) const {
+        USHORT buttonCapsLen = 0;
+        if (type == HidP_Input)
+            buttonCapsLen = caps.NumberInputButtonCaps;
+        else if (type == HidP_Output)
+            buttonCapsLen = caps.NumberOutputButtonCaps;
+        else if (type == HidP_Feature)
+            buttonCapsLen = caps.NumberFeatureButtonCaps;
+        if (buttonCapsLen == 0)
+            return {};
+
+        std::vector<HIDP_BUTTON_CAPS> buttonCaps(buttonCapsLen, {});
+        NTSTATUS status = HidP_GetButtonCaps(type, buttonCaps.data(), &buttonCapsLen, preparsed);
+        if (status == HIDP_STATUS_INVALID_PREPARSED_DATA) {
+            wprintf(L"WARNING: Invalid preparsed data.\n");
+            return {};
+        }
+
+        assert(status == HIDP_STATUS_SUCCESS); status;
+        return buttonCaps;
+    }
+
+    /** Return a list of unique ReportID values found in the input. */
+    template <class T> // T might be HIDP_VALUE_CAPS or HIDP_BUTTON_CAPS
+    static std::vector<UCHAR> GetReportIDs(const std::vector<HIDP_BUTTON_CAPS> input) {
+        std::vector<UCHAR> result;
+        for (auto& elm : input)
+            result.push_back(elm.ReportID);
+
+        // sort and remove duplicates
+        std::sort(result.begin(), result.end());
+        result.erase(std::unique(result.begin(), result.end()), result.end());
+
+        return result;
     }
 
     std::wstring Name() const {
