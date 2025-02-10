f7c4010be83c2e747b4a419685810743552a2bc1
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-10 20:08:45 +0100

    Update TailLight report structure and HidUtil project so that it reads the Temperature in Kelvin.

--- HidUtil/TailLight.hpp
+++ HidUtil/TailLight.hpp
@@ -1,24 +1,11 @@
 #pragma once
 
 
-bool SetTailLightRGB(hid::Device& dev, COLORREF color) {
+bool SetTailLightRGB(hid::Device& dev) {
     {
         // query current color (doesn't work)
-        auto report = dev.GetReport<TailLightReport>(HidP_Feature);
-        wprintf(L"Current color: %u\n", report.GetColor());
-    }
-
-    {
-        // update color
-        TailLightReport report;
-        report.SetColor(color);
-        bool ok = dev.SetReport(HidP_Feature, report);
-        if (!ok) {
-            wprintf(L"ERROR: Set TailLightReport failure.\n");
-            assert(ok);
-            return false;
-        }
-        wprintf(L"SUCCESS: Tail-light color updated.\n");
+        auto report = dev.GetReport<TailLightReport>(HidP_Feature, TailLightReport::Temperature);
+        wprintf(L"Current temp: %u\n", report.Value);
     }
 
     return true;
