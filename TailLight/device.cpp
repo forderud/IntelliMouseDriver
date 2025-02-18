544bd4242e90114ca4392b35e3214520306dae15
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-18 19:11:43 +0100

    Remove filter queues since this is no longer a "filter" driver.

--- TailLight/device.cpp
+++ TailLight/device.cpp
@@ -4,8 +4,6 @@
 #include "wmi.hpp"
 
 
-EVT_WDF_IO_QUEUE_IO_DEVICE_CONTROL EvtIoDeviceControlFilter;
-
 
 VOID PollInputReportsTimer(_In_ WDFTIMER  Timer) {
     DebugEnter();
@@ -133,23 +131,6 @@ Arguments:
         DebugPrint(DPFLTR_INFO_LEVEL, "TailLight: PdoName: %wZ\n", deviceContext->PdoName); // outputs "\Device\00000083"
     }
 
-    {
-        // create queue for filtering
-        WDF_IO_QUEUE_CONFIG queueConfig = {};
-        WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&queueConfig, WdfIoQueueDispatchParallel); // don't synchronize
-        //queueConfig.EvtIoRead  // pass-through read requests 
-        //queueConfig.EvtIoWrite // pass-through write requests
-        queueConfig.EvtIoDeviceControl = EvtIoDeviceControlFilter; // filter IOCTL requests
-
-        WDFQUEUE queue = 0; // auto-deleted when parent is deleted
-        NTSTATUS status = WdfIoQueueCreate(Device, &queueConfig, WDF_NO_OBJECT_ATTRIBUTES, &queue);
-
-        if (!NT_SUCCESS(status)) {
-            DebugPrint(DPFLTR_ERROR_LEVEL, DML_ERR("TailLight: WdfIoQueueCreate failed 0x%x"), status);
-            return status;
-        }
-    }
-
     // Initialize WMI provider
     NTSTATUS status = WmiInitialize(Device);
     if (!NT_SUCCESS(status)) {
@@ -160,59 +141,4 @@ Arguments:
     KeInitializeEvent(&deviceContext->SelfTestCompleted, SynchronizationEvent, FALSE);
 
     return status;
-}
-
-
-VOID EvtIoDeviceControlFilter(
-    _In_  WDFQUEUE          Queue,
-    _In_  WDFREQUEST        Request,
-    _In_  size_t            OutputBufferLength,
-    _In_  size_t            InputBufferLength,
-    _In_  ULONG             IoControlCode
-)
-/*++
-Routine Description:
-    This event callback function is called when the driver receives an
-
-    (KMDF) IOCTL_HID_Xxx code when handling IRP_MJ_INTERNAL_DEVICE_CONTROL
-    (UMDF) IOCTL_HID_Xxx, IOCTL_UMDF_HID_Xxx when handling IRP_MJ_DEVICE_CONTROL
-
-Arguments:
-    Queue - A handle to the queue object that is associated with the I/O request
-
-    Request - A handle to a framework request object.
-
-    OutputBufferLength - The length, in bytes, of the request's output buffer,
-            if an output buffer is available.
-
-    InputBufferLength - The length, in bytes, of the request's input buffer, if
-            an input buffer is available.
-
-    IoControlCode - The driver or system defined IOCTL associated with the request
---*/
-{
-    UNREFERENCED_PARAMETER(OutputBufferLength);
-
-    //DebugPrint(DPFLTR_INFO_LEVEL, "TailLight: EvtIoDeviceControl (IoControlCode=0x%x, InputBufferLength=%Iu)\n", IoControlCode, InputBufferLength);
-
-    WDFDEVICE Device = WdfIoQueueGetDevice(Queue);
-
-    NTSTATUS status = STATUS_SUCCESS; //unhandled
-    switch (IoControlCode) {
-      case IOCTL_HID_SET_FEATURE: // 0xb0191
-        status = SetFeatureFilter(Device, Request, InputBufferLength);
-        break;
-    }
-    // No NT_SUCCESS(status) check here since we don't want to fail blocked calls
-
-    // Forward the request down the driver stack
-    WDF_REQUEST_SEND_OPTIONS options = {};
-    WDF_REQUEST_SEND_OPTIONS_INIT(&options, WDF_REQUEST_SEND_OPTION_SEND_AND_FORGET);
-
-    BOOLEAN ret = WdfRequestSend(Request, WdfDeviceGetIoTarget(Device), &options);
-    if (ret == FALSE) {
-        status = WdfRequestGetStatus(Request);
-        DebugPrint(DPFLTR_ERROR_LEVEL, DML_ERR("TailLight: WdfRequestSend failed with status: 0x%x"), status);
-        WdfRequestComplete(Request, status);
-    }
 }
