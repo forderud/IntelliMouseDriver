0c2acaa11b93ad75c20e571f79d028bba5129bab
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-20 13:12:26 +0100

    WMI removal follow-up

--- TailLight/UNINSTALL_TailLight.bat
+++ TailLight/UNINSTALL_TailLight.bat
@@ -5,7 +5,4 @@ cd /d "%~dp0"
 :: uninstall driver
 PNPUTIL /delete-driver TailLight.inf /uninstall /force
 
-:: Delete TailLightDeviceInformation WMI class security descriptor
-reg delete "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Security" /v "07982702-2086-4B83-9444-34989BB10554" /f
-
 pause
