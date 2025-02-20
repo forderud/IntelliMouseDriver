deff3f40a1399250934aff141a35c035add4ec32
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-20 09:54:54 +0100

    Introduce SharedState struct (not yet populated).

--- TailLight/device.h
+++ TailLight/device.h
@@ -5,10 +5,17 @@ enum FilterMode {
     LowerFilter, // below HidBatt: Filters HID Power Device communication
 };
 
+/** State to share between Upper and Lower filter driver instances. */
+struct SharedState {
+    ULONG CycleCount; // BATTERY_INFORMATION::CycleCount value
+    ULONG Temperature; // IOCTL_BATTERY_QUERY_INFORMATION BatteryTemperature value
+};
+
 /** Driver-specific struct for storing instance-specific data. */
 struct DEVICE_CONTEXT {
     FilterMode     Mode;
     UNICODE_STRING PdoName;
+    SharedState    State;
 };
 WDF_DECLARE_CONTEXT_TYPE(DEVICE_CONTEXT)
 
