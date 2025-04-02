4ac8ba58ee330bc41745ce6dd0cb58c58a587c0c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-04-02 13:41:59 +0200

    WIP: Adapt device search to HID Battery Devices

--- HidUtil/Main.cpp
+++ HidUtil/Main.cpp
@@ -11,22 +11,16 @@ void PrintReport(const std::vector<BYTE>& report) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 4) {
-        wprintf(L"IntelliMouse tail-light shifter.\n");
-        wprintf(L"Usage: \"HidUtil.exe <red> <green> <blue>\" (example: \"HidUtil.exe 0 0 255\").\n");
-        return -1;
-    }
-
-    BYTE red = (BYTE)atoi(argv[1]);
-    BYTE green = (BYTE)atoi(argv[2]);
-    BYTE blue = (BYTE)atoi(argv[3]);
+    BYTE red = 0;
+    BYTE green = 0;
+    BYTE blue = 0;
 
     hid::Scan::Criterion crit;
     //crit.VendorID = 0x045E;  // Microsoft
     //crit.ProductID = 0x082A; // Pro IntelliMouse
     //crit.ProductID = 0x0047; // IntelliMouse Explorer 3.0 (access denied)
-    crit.Usage = 0x0212;     //
-    crit.UsagePage = 0xFF07; // Vendor-defined range (FF00-FFFF)
+    crit.Usage = 0x04;     //
+    crit.UsagePage = 0x84; // Vendor-defined range (FF00-FFFF)
 
     wprintf(L"Searching for matching HID devices...\n");
     std::vector<hid::Device> matches = hid::Scan::FindDevices(crit, false);
